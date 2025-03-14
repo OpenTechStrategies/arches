d7800405c422a94d74a36d551a0054dfd0685d3a
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-03-14 09:12:46 +0000

    update migration path

--- arches/app/models/migrations/11842_userpreferences.py
+++ arches/app/models/migrations/11842_userpreferences.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "11726_join_tile_nodegroup"),
+        ("models", "11570_harden_editable_future_graphs"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
