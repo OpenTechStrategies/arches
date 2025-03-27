fab588173b4b1609933c8b599278b447f41106c7
Christopher Byrd <chrabyrd@gmail.com>
2025-03-27 15:59:44 -0700

    nit

--- arches/app/media/js/bindings/codemirror.js
+++ arches/app/media/js/bindings/codemirror.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 import ko from 'knockout';
+import codemirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
 
-
-ko.bindingHandlers.codemirror = function (element, valueAccessor, CodeMirror) {
+const initialize = function(element, valueAccessor, CodeMirror) {
     var options = ko.toJS(valueAccessor());
     options.value = options.value || '';
     var editor = new CodeMirror(element, options);
-    editor.on('change', function (cm) {
+    editor.on('change', function(cm) {
         var value = ko.unwrap(valueAccessor()).value;
         if (ko.isObservable(value)) {
             value(cm.getValue());
@@ -16,24 +17,31 @@ ko.bindingHandlers.codemirror = function (element, valueAccessor, CodeMirror) {
     });
     var subscriptions = [];
     if (ko.isObservable(valueAccessor().value)) {
-        subscriptions.push(valueAccessor().value.subscribe(function () {
+        subscriptions.push(valueAccessor().value.subscribe(function() {
             if (editor.getValue() !== valueAccessor().value())
                 editor.setValue(valueAccessor().value());
         }));
     }
     if (ko.isObservable(valueAccessor().mode)) {
-        subscriptions.push(valueAccessor().mode.subscribe(function () {
+        subscriptions.push(valueAccessor().mode.subscribe(function() {
             editor.setOption('mode', valueAccessor().mode());
         }));
     }
 
     var wrapperElement = $(editor.getWrapperElement());
-    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+    ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
         wrapperElement.remove();
         for (var i = 0; i < subscriptions.length; i++) {
             subscriptions[i].dispose();
         }
     });
 };
+
+ko.bindingHandlers.codemirror = {
+    init: (element, valueAccessor) => {
+        initialize(element, valueAccessor, codemirror);
+    }
+};
+ko.bindingHandlers.codemirror.init = ko.bindingHandlers.codemirror.init.bind(ko.bindingHandlers.codemirror);
 
 export default ko.bindingHandlers.codemirror;
