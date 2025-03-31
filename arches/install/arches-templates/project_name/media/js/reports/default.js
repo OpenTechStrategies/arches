97fc9d288b893b8be8eae5b3b5258683c7758ef8
Christopher Byrd <chrabyrd@gmail.com>
2025-03-31 13:08:54 -0700

    nit

--- arches/install/arches-templates/project_name/media/js/reports/default.js
+++ arches/install/arches-templates/project_name/media/js/reports/default.js
@@ -2,7 +2,7 @@ import ko from 'knockout';
 import ReportViewModel from 'viewmodels/report';
 import defaultReportTemplate from 'templates/views/report-templates/default.htm';
 
-ko.components.register('default-report', {
+export default ko.components.register('default-report', {
     viewModel: function (params) {
         params.configKeys = [];
         ReportViewModel.apply(this, [params]);
