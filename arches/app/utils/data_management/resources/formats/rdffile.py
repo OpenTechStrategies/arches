a1d09e7bafcc60f13895e62778f55eb07d1080f1
Margarita <margipsi96@gmail.com>
2025-08-01 16:50:14 +0300

    Renamed k1 and v to nested_key and nested_value in inner loop

--- arches/app/utils/data_management/resources/formats/rdffile.py
+++ arches/app/utils/data_management/resources/formats/rdffile.py
@@ -890,9 +890,9 @@ class JsonLdReader(Reader):
                     try:
                         self.printline("Found multiple matches!", indent)
                         # if this doesn't throw an error then keep the possible branch "p"
-                        for k1, v in vi.items():
+                        for nested_key, nested_value in vi.items():
                             matched_branch = self.find_matching_branch(
-                                k1, v, p[0], {}, tile, indent + 1
+                                nested_key, nested_value, p[0], {}, tile, indent + 1
                             )
                         possible2.append(p)
                     except Exception as e:
